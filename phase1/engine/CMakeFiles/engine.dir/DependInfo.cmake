58ecb7554b0431939345755574eeba2f1a70c6f7
Samuel de Almeida Simoes Lira <sdasl03@github.com>
2022-03-11 09:40:43 +0000

    esfera quase la

--- phase1/engine/CMakeFiles/engine.dir/DependInfo.cmake
+++ phase1/engine/CMakeFiles/engine.dir/DependInfo.cmake
@@ -4,13 +4,8 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-<<<<<<< HEAD
   "/home/sdaslira/Projeto-CG-21-22/phase1/engine/engine.cpp" "/home/sdaslira/Projeto-CG-21-22/phase1/engine/CMakeFiles/engine.dir/engine.cpp.o"
   "/home/sdaslira/Projeto-CG-21-22/phase1/engine/tinyxml2.cpp" "/home/sdaslira/Projeto-CG-21-22/phase1/engine/CMakeFiles/engine.dir/tinyxml2.cpp.o"
-=======
-  "/home/sleiman/Projeto-CG-21-22/phase1/engine/engine.cpp" "/home/sleiman/Projeto-CG-21-22/phase1/engine/CMakeFiles/engine.dir/engine.cpp.o"
-  "/home/sleiman/Projeto-CG-21-22/phase1/engine/tinyxml2.cpp" "/home/sleiman/Projeto-CG-21-22/phase1/engine/CMakeFiles/engine.dir/tinyxml2.cpp.o"
->>>>>>> 1d4f474e735a3c9e93c67b4f753caf365b2c0d9f
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
