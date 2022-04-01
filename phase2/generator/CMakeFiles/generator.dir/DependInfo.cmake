c17d2b28ef6c28b9f0cfb41d58defbfec0f79fb3
Samuel de Almeida Simoes Lira <sdasl03@github.com>
2022-04-01 08:48:08 +0100

    planetas quase!

--- phase2/generator/CMakeFiles/generator.dir/DependInfo.cmake
+++ phase2/generator/CMakeFiles/generator.dir/DependInfo.cmake
@@ -4,8 +4,8 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/ema/Projeto-CG-21-22/phase2/generator/generator.cpp" "/home/ema/Projeto-CG-21-22/phase2/generator/CMakeFiles/generator.dir/generator.cpp.o"
-  "/home/ema/Projeto-CG-21-22/phase2/generator/generator_aux.cpp" "/home/ema/Projeto-CG-21-22/phase2/generator/CMakeFiles/generator.dir/generator_aux.cpp.o"
+  "/home/sdaslira/Projeto-CG-21-22/phase2/generator/generator.cpp" "/home/sdaslira/Projeto-CG-21-22/phase2/generator/CMakeFiles/generator.dir/generator.cpp.o"
+  "/home/sdaslira/Projeto-CG-21-22/phase2/generator/generator_aux.cpp" "/home/sdaslira/Projeto-CG-21-22/phase2/generator/CMakeFiles/generator.dir/generator_aux.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
